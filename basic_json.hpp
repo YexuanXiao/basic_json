9d8a94ad39ab16d7bd5c10d97e885d389e8538ec
YexuanXiao <bizwen@nykz.org>
2024-02-04 02:31:12 +0800

    Update basic_json.hpp

--- basic_json.hpp
+++ basic_json.hpp
@@ -1,14 +1,22 @@
-#include <string>
 #include <map>
 #include <memory>
+#include <string>
 #include <vector>
+
 // https://www.rfc-editor.org/rfc/rfc8259
 
 namespace bizwen
 {
+    struct nulljson_t
+    {
+        explicit constexpr nulljson_t(decltype(nullptr)) {}
+    };
 
-    template <typename Boolean = bool, typename Number = double, typename Integer = long long, typename UInteger = unsigned long long>
-    struct json_handle
+    inline constexpr nulljson_t nulljson{ nullptr };
+
+    template <typename Boolean = bool, typename Number = double,
+        typename Integer = long long, typename UInteger = unsigned long long>
+    struct json_node
     {
         using boolean_t = Boolean;
         using number_t = Number;
@@ -17,111 +25,316 @@ namespace bizwen
 
         enum class kind_t : unsigned char
         {
+            empty,
             null,
-            boolean,
+            true_value,
+            false_value,
             number,
+            integer,
+            uinteger,
             string,
             array,
-            object,
-            integer,
-            uinteger
+            object
         };
 
         union storage_type_
         {
-            decltype(nullptr) null_;
-            boolean_t bool_;
+            void* str_;
+            void* arr_;
+            void* obj_;
             number_t num_;
             integer_t int_;
             uinteger_t uint_;
-            void* ptr_;
         };
 
-        storage_type_ stor_;
-        kind_t kind_;
+        storage_type_ stor_{};
+        kind_t kind_{};
+
+        constexpr json_node() noexcept = default;
+        constexpr json_node(json_node const&) = delete;
+        constexpr json_node(json_node&& rhs) noexcept
+        {
+            {
+                auto temp = stor_;
+                stor_ = rhs.stor_;
+                rhs.stor_ = temp;
+            }
+            {
+                auto temp = kind_;
+                kind_ = rhs.kind_;
+                rhs.kind_ = temp;
+            }
+        }
     };
 
-    template <typename Handle = json_handle<>, typename String = std::string, typename Array = std::vector<json_handle<>>, typename Map = std::map<std::string, json_handle<>>, typename Allocator = std::allocator<json_handle<>>, bool has_integer = true, bool has_uinteger = true>
-    class basic_json
+    template <typename node = json_node<>, typename String = std::string,
+        typename Array = std::vector<json_node<>>,
+        typename Map = std::map<std::string, json_node<>>,
+        typename Allocator = std::allocator<json_node<>>,
+        bool has_integer = true, bool has_uinteger = true>
+    struct basic_json;
+
+    template <typename Node = json_node<>, typename String = std::string,
+        typename Array = std::vector<json_node<>>,
+        typename Map = std::map<std::string, json_node<>>,
+        typename Allocator = std::allocator<json_node<>>,
+        bool has_integer = true, bool has_uinteger = true>
+    struct basic_const_json_span
     {
-        using handle_t = Handle;
-        using number_t = handle_t::number_t;
-        using boolean_t = handle_t::boolean_t;
-        using integer_t = handle_t::integer_t;
-        using uinteger_t = handle_t::uinteger_t;
+        using node_t = Node;
+        using number_t = node_t::number_t;
+        using boolean_t = node_t::boolean_t;
+        using integer_t = node_t::integer_t;
+        using uinteger_t = node_t::uinteger_t;
+        using string_t = String;
+        using object_t = Map;
+        using array_t = Array;
+        using allocator_t = Allocator;
+        using kind_t = node_t::kind_t;
+        using json_t = basic_json<node_t, string_t, array_t, object_t, allocator_t, has_integer, has_uinteger>;
+
+        json_t* json_{};
+
+        [[nodiscard]] constexpr kind_t kind() const noexcept
+        {
+            return json_->node_.kind_;
+        }
+        [[nodiscard]] constexpr node_t::storage_type_& stor() noexcept
+        {
+            return json_->node_.stor_;
+        }
+        [[nodiscard]] constexpr node_t::storage_type_ const& stor() const noexcept
+        {
+            return json_->node_.stor_;
+        }
+        [[nodiscard]] constexpr bool empty() const noexcept
+        {
+            return kind() == kind_t::empty;
+        }
+        [[nodiscard]] constexpr bool string() const noexcept
+        {
+            return kind() == kind_t::string;
+        }
+        [[nodiscard]] constexpr bool null() const noexcept
+        {
+            return kind() == kind_t::null;
+        }
+        [[nodiscard]] constexpr bool boolean() const noexcept
+        {
+            auto k = kind();
+            return k == kind_t::true_value || kind_t::false_value;
+        }
+        [[nodiscard]] constexpr bool number() const noexcept
+        {
+            return kind() == kind_t::number;
+        }
+        [[nodiscard]] constexpr bool object() const noexcept
+        {
+            return kind() == kind_t::object;
+        }
+        [[nodiscard]] constexpr bool array() const noexcept
+        {
+            return kind() == kind_t::array;
+        }
+        [[nodiscard]] constexpr bool integer() const noexcept
+            requires(has_integer)
+        {
+            return kind() == kind_t::integer;
+        }
+        [[nodiscard]] constexpr bool uinteger() const noexcept
+            requires(has_uinteger)
+        {
+            return kind() == kind_t::uinteger;
+        }
+
+        constexpr void swap(basic_const_json_span& rhs) noexcept
+        {
+            auto temp = json_;
+            json_ = rhs.json_;
+            rhs.json_ = temp;
+        }
+
+        friend constexpr void swap(basic_const_json_span& lhs, basic_const_json_span& rhs) noexcept
+        {
+            lhs.swap(rhs);
+        }
+
+        constexpr basic_const_json_span() noexcept = default;
+        constexpr basic_const_json_span(basic_const_json_span&& rhs) noexcept = default;
+        constexpr basic_const_json_span(json_t& j) noexcept
+            : json_(&j)
+        {
+        }
+        constexpr basic_const_json_span(json_t const& j) noexcept
+            : json_(&j)
+        {
+        }
+        basic_const_json_span(node_t const& n) noexcept
+            : json_(reinterpret_cast<json_t*>(n))
+        {
+        }
+        constexpr explicit operator boolean_t() const noexcept
+        {
+            // todo: replace all asserts as exceptions
+            assert(boolean());
+            auto k = kind();
+            return k == kind_t::true_value ? 1 : 0;
+        }
+        constexpr explicit operator number_t() const noexcept
+        {
+            assert(number());
+            return json_->stor_.num_;
+        }
+        constexpr explicit operator nulljson_t() const noexcept
+        {
+            assert(null());
+            return nulljson;
+        }
+        constexpr explicit operator const string_t&() const& noexcept
+        {
+            assert(string());
+            return *(json_->stor_.str_);
+        }
+        constexpr explicit operator const array_t&() const& noexcept
+        {
+            assert(array());
+            return *(json_->stor_.arr_);
+        }
+        constexpr explicit operator const object_t&() const& noexcept
+        {
+            assert(object());
+            return *(json_->stor_.obj_);
+        }
+        constexpr explicit operator integer_t() const noexcept
+            requires(has_integer)
+        {
+            assert(integer());
+            return json_->stor_.int_;
+        }
+        constexpr explicit operator uinteger_t() const noexcept
+            requires(has_uinteger)
+        {
+            assert(uinteger());
+            return json_->stor_.uint_;
+        }
+    };
+
+    template <typename node, typename String,
+        typename Array,
+        typename Map,
+        typename Allocator,
+        bool has_integer, bool has_uinteger>
+    struct basic_json
+    {
+        using node_t = node;
+        using number_t = node_t::number_t;
+        using boolean_t = node_t::boolean_t;
+        using integer_t = node_t::integer_t;
+        using uinteger_t = node_t::uinteger_t;
         using null_t = decltype(nullptr);
         using string_t = String;
         using object_t = Map;
         using array_t = Array;
-        using kind_t = handle_t::kind_t;
+        using allocator_t = Allocator;
+        using kind_t = node_t::kind_t;
 
-        handle_t handle_;
+        node_t node_{};
 
-        void allocate(kind_t k)
+        void dealloc() noexcept
         {
-            handle_.kind_ = k;
-            auto&& stor_ = handle_.stor_;
+            auto k = node_.kind_;
+            auto&& stor_ = node_.stor_;
 
             switch (k)
             {
-            case kind_t::null: {
-                stor_.null_ = nullptr;
-                break;
-            }
-            case kind_t::boolean: {
-                stor_.bool_ = boolean_t{};
-                break;
-            }
-            case kind_t::number: {
-                stor_.num_ = number_t{};
-                break;
-            }
             case kind_t::string: {
-                stor_.str_ = new string_t{};
+                delete (stor_.str_);
                 break;
             }
             case kind_t::array: {
-                stor_.arr_ = new array_t{};
+                delete (stor_.arr_);
                 break;
             }
             case kind_t::object: {
-                stor_.obj_ = new object_t{};
-                break;
-            }
-            case kind_t::integer: {
-                stor_.int_ = integer_t{};
-                break;
-            }
-            case kind_t::uinteger: {
-                stor_.uint_ = uinteger_t{};
+                delete (stor_.obj_);
                 break;
             }
             }
         }
 
-        void dealloc(kind_t k)
+        constexpr void kind(kind_t k) noexcept { node_.kind_ = k; }
+        [[nodiscard]] constexpr kind_t kind() const noexcept
         {
-            handle_.kind_ = k;
-            auto&& stor_ = handle_.stor_;
+            return node_.kind_;
+        }
+        [[nodiscard]] constexpr node_t::storage_type_& stor() noexcept
+        {
+            return node_.stor_;
+        }
+        [[nodiscard]] constexpr node_t::storage_type_ const& stor() const noexcept
+        {
+            return node_.stor_;
+        }
 
-            switch (k)
-            {
-            case kind_t::string: {
-                delete (stor_.ptr_);
-                break;
-            }
-            case kind_t::array: {
-                delete (stor_.ptr_);
-                break;
-            }
-            case kind_t::object: {
-                delete (stor_.ptr_);
-                break;
-            }
-            }
+        constexpr void swap(basic_json& rhs) noexcept
+        {
+            auto temp = node_;
+            node_ = rhs.node_;
+            rhs.node_ = temp;
+        }
+
+        friend constexpr void swap(basic_json& lhs, basic_json& rhs) noexcept
+        {
+            lhs.swap(rhs);
+        }
+
+        constexpr basic_json() noexcept = default;
+        constexpr basic_json(basic_json&& rhs) noexcept
+        {
+            rhs.swap(*this);
+        }
+        constexpr basic_json(decltype(nullptr)) = delete; // prevent implicit construct string
+        constexpr basic_json(boolean_t v) noexcept
+        {
+            stor().bool_ = v;
+            kind(kind_t::boolean);
+        }
+        constexpr basic_json(string_t v) noexcept
+        {
+            stor().str_ = new string_t(std::move(v));
+            kind(kind_t::string);
+        }
+        constexpr basic_json(number_t v) noexcept
+        {
+            stor().num_ = v;
+            kind(kind_t::number);
+        }
+        constexpr basic_json(integer_t v) noexcept
+            requires(has_integer)
+        {
+            stor().int_ = v;
+            kind(kind_t::integer);
+        }
+        constexpr basic_json(uinteger_t v) noexcept
+            requires(has_uinteger)
+        {
+            stor().uint_ = v;
+            kind(kind_t::integer);
+        }
+        constexpr basic_json(node_t&& n) noexcept
+        {
+            // todo: destory this
+            node_ = node_t{};
+            node_ = std::move(n);
+        }
+        constexpr operator node_t() && noexcept
+        {
+            auto temp = node_;
+            node_ = node_t{};
+            return temp;
         }
     };
 
     using json = basic_json<>;
 
-}
+} // namespace bizwen
